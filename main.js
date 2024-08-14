5114700c52732076407e96866ec6afc4b42d4d0b
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-08-14 19:47:06 -0300

    atualização js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}` 
+    const idAudio = `#som_${instrumento}`; 
 
     tecla.onclick = function () {
         tocaSom(idAudio);
